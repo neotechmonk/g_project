b856bd93823defb790cbd9bc40dd3726e07f0350
your_username <your_email@gmail.com>
2023-03-08 22:31:10 +1100

    print statements  to get started

---
+++ brode_code/src/1_intro.py
@@ -0,0 +1,18 @@
+# Basic convention 
+print ("I am setup")
+
+
+
+#using format to pass variables
+pet = "snake"
+print("My name is {} and I am loving the {} years old.".format("Neotech Monk", pet))
+
+
+#using format method
+pet = "snake"
+print("My name is {} and I am loving the {} years old.".format("Neotech Monk", pet))
+
+city = "London"
+activity = "partying like the royal"
+# string interpolation . Not the f at the beginning
+print(f"Who doesn't like to like to live in {city}. Its the best for {activity}")
