cc29fda9c2e07c94d537c0cc123225896ead4039
your_username <your_email@gmail.com>
2023-03-11 08:55:39 +1100

    User input and manipulation

---
+++ bro_code/src/5_user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+age=  int(age) +1 # Input sources data as string.. Need to type cast to perform math
+
+
+print(f'Hello {name}. Are you sure you are not {age}?')
