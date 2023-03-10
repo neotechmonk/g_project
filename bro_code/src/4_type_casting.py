95df3bb6bfcff28e82cc055de4978e57f87a2fa3
your_username <your_email@gmail.com>
2023-03-11 08:46:22 +1100

    Type casting

---
+++ bro_code/src/4_type_casting.py
@@ -0,0 +1,20 @@
+x, y, z = 1, 4.6565, "9"
+print(x)
+print(y)
+print(z)
+
+# float to int
+y_int = int(y)
+print(y_int)
+
+#Cant perform math on str
+print (z *3) #Not 27 
+
+
+z_int = int (z)
+print (z_int *3) #27 
+
+
+# int to float 
+x_float = float(x)
+print(x_float)
