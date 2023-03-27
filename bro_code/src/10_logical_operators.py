76b274346fc4232c02f258e238c0e19f02f5a2db
your_username <your_email@gmail.com>
2023-03-27 14:14:15 +1100

    any and all

--- bro_code/src/10_logical_operators.py
+++ bro_code/src/10_logical_operators.py
@@ -2,6 +2,10 @@
 and - all conditions  
 or - at least one of the  conditions
 
+additional for collections 
+    - any - some 
+    - all  - all 
+
 """
 
 a, b = 5, 4
@@ -16,3 +20,24 @@ elif (a == 5 or b == 0): ## b == 0 is False
 
 if not(a is None and b is None ):
     print (f"{a} and {b} are not None")
+
+
+
+
+# all
+
+my_list = [True, False, True, True]
+
+if all(my_list):
+    print("All elements in the list are True")
+else:
+    print("Not all elements in the list are True")
+
+# any
+my_list = [False, False, True, False]
+
+if any(my_list):
+    print("At least one element in the list is True")
+else:
+    print("No elements in the list are True")
+
